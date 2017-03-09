40bdeb77a1a619eb13321cfb2d9cca82a17b184f
currentsea <currentsea@currentsea.io>
2017-03-08 22:42:27 -0800

    add player override mechanims

--- scripts/kittyhunter.js
+++ scripts/kittyhunter.js
@@ -36,6 +36,8 @@ var multipier = 100;
 var targetbonus = 0; 
 var liveplay = true; 
 var playeroverridecount = 0; 
+var playeroverride = false; 
+var gamecount = 0; 
 var game_records = {}; 
 var player_record = {}; 
 var curgameid = 0; 
@@ -74,9 +76,9 @@ function place_bet(game_data) {
 	var formattedmultiplier = multiplier / 100.0; 
 	var targetprofit = formattedmultiplier * currentbet; 
 	var numplayersingame = game_data.length; 
-	console.log(numplayersingame + ' players are in this game'); 
+	// console.log(numplayersingame + ' players are in this game'); 
 	if (liveplay == true) { 
-		console.log("---| Placing Bet: " + formatamount(currentbet) + "bits with a cashout multiplier of " + formattedmultiplier + "x (Target Profit: " + targetprofit + "bits)"); 
+		console.log("---| Placing Bet: " + currentbet + "bits with a cashout multiplier of " + formattedmultiplier + "x (Target Profit: " + targetprofit + "bits)"); 
 		engine.placeBet(formatamount(currentbet), multiplier, false);  
 	} else { 
 		// console.log('---| Placing Paper Bet: ' + currentbet + ' bits with a cashout multiplier of ' + formattedmultiplier); 
@@ -85,7 +87,12 @@ function place_bet(game_data) {
 
 function cash_out(game_data) { 
 	game_records[current_game_guid]['live_game_data']['cashouts'].push(game_data); 
-
+	if (game_data.username == engine.getUsername() && game_data.stopped_at < multiplier) { 
+		var ourbet = game_records[current_game_guid]['live_game_data']['initial_bets'][engine.getUsername()]['bet'] / 100; 
+		console.log("---| Player override cashout detected: " + ourbet + 'bits @' + game_data.stopped_at / 100.0 + 'x'); 
+		playeroverride = true; 
+		playeroverridecount++; 
+	} 
 } 
 
 function play_game(game_data) { 
@@ -97,14 +104,15 @@ function play_game(game_data) {
 	if (playerbet != currentbet) { 
 		currentbet = playerbet; 
 		playeroverridecount++; 
-		console.log('---| Player override detected - bet of ' + playerbet + ' bits (target bonus: ' + targetbonus + ' bits)'); 
+		// console.log('---| Player override detected - bet of ' + playerbet + ' bits (target bonus: ' + targetbonus + ' bits)'); 
 	}
-	console.log('MY TARGET BONUS IS: ' + targetbonus + ' bits');
-	console.log(JSON.stringify(targetbonus));   
+	// console.log('MY TARGET BONUS IS: ' + targetbonus + ' bits');
+	// console.log(JSON.stringify(targetbonus));   
 	game_records[current_game_guid]['live_game_data']['initial_bets'] = game_data; 
 }
 
 function finish_game(game_data) { 
+	gamecount++; 
 	var gamecrash = game_data.game_crash; 
 	var mybonus = game_data['bonuses'][engine.getUsername()] / 100; 
 
@@ -116,7 +124,8 @@ function finish_game(game_data) {
 	} 
 
 	gamecrash = gamecrash / 100.0; 
-	console.log('---| Game #' + curgameid + ' (' + current_game_guid + ') is over.'); 
+	console.log('---| Game #' + curgameid + ' (' + current_game_guid + ') is over. (Game Count: ' + gamecount
+		 + ")"); 
 	console.log('---| Game Crash: '  + gamecrash + 'x'); 
 	console.log('---| My Bonus: '  + mybonus + 'bits'); 
 	console.log(JSON.stringify(game_data)); 
@@ -125,9 +134,16 @@ function finish_game(game_data) {
 	if (playresult == 'NOT_PLAYED') { 
 		console.log('Didnt play'); 
 	} else if (playresult == ('WON')) { 
-		console.log("WE SNIPED THE KITTY.  TIME TO BAG IT AND TAG IT."); 
-		console.log("STOPPING $CRIPT"); 
-		engine.stop(); 
+		if (playeroverride == true) { 
+			console.log("---| A human manually intervened and we won the game.  Not stopping the script.")
+		} else { 
+			console.log("WE SNIPED THE KITTY.  TIME TO BAG IT AND TAG IT."); 
+			console.log("STOPPING $CRIPT"); 
+			console.log("FINAL GAME RECORDS SHOWN BELOW"); 
+			console.log(JSON.stringify(game_records)); 
+			console.log(JSON.stringify(player_record));
+			engine.stop(); 
+		} 
 	} else { 
 		console.log("We lost."); 
 		gameslost++; 
@@ -136,10 +152,14 @@ function finish_game(game_data) {
 
 	if (gameslost % 100 == 0) { 
 		console.log('Another 100 games of loss detected - resetting teh sniper'); 
+		console.log(JSON.stringify(game_records)); 
+		console.log(JSON.stringify(player_record)); 
+		console.log("NUMBER OF PLAYER OVERRIDES: " + playeroverridecount); 
 		currentbet = initialbet + (gameslost / 100.0); 
 		initialbet += (gameslost / 100.0); 
 	} 
 	current_game_guid = guid(); 
+	playeroverride = false; 
 }
 
 
--- scripts/run.py
+++ scripts/run.py
@@ -1,7 +1,7 @@
 start = 1
 current = start 
 total = 0 
-for x in range(0, 5000): 
+for x in range(0, 500): 
 	if x % 100 == 0: 
 		current = start + (x / 100.0)
 	else: 
